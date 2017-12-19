07e1af8bec2f8231e94b718bf70d2f120a31c10f
DovahChief <pepelimonta@gmail.com>
2017-12-19 12:07:04 -0600

    Delimitadores parametrizados Epsilon

--- src/main/java/com/sixdelta/EpsilonStringCalculator.java
+++ src/main/java/com/sixdelta/EpsilonStringCalculator.java
@@ -7,7 +7,14 @@ public class EpsilonStringCalculator {
 
         if(s.isEmpty()) return 0;
 
-    	return  Stream.of(s.split(",|\\n"))
+        String delim = ",|\\n";
+
+        if(s.startsWith("//")) {
+            delim = s.substring(2,s.indexOf("\n") );
+            s = s.substring(s.indexOf("\n")+1);
+        }
+
+    	return  Stream.of(s.split(delim))
                       .mapToInt(Integer::parseInt)
                       .sum();
     }
