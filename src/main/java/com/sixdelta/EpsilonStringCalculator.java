8b48bbb282c2fee398e0f85696468cfe2ccebbf3
Alberto Hernandez <alberto.jhs.33@gmail.com>
2017-12-19 11:22:05 -0600

    Prueba dos epsilon

--- src/main/java/com/sixdelta/EpsilonStringCalculator.java
+++ src/main/java/com/sixdelta/EpsilonStringCalculator.java
@@ -1,5 +1,14 @@
 package com.sixdelta;
 
+import java.util.stream.Stream;
+
 public class EpsilonStringCalculator {
-    public int add(String s){return 1;}
+    public int add(String s){
+    	return  Stream.of(s.split(","))
+                      .mapToInt(Integer::parseInt)
+                      .sum();
+
+
+    	
+    }
 }
