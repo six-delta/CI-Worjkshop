cdd5bdad7b4c3219b2ac6a7abbac194c8568d60f
ALfonso <alvaco_1993@hotmail.com>
2017-12-19 15:18:07 -0600

    Actualizacion Alfa

--- src/main/java/com/sixdelta/AlfaStringCalculator.java
+++ src/main/java/com/sixdelta/AlfaStringCalculator.java
@@ -4,13 +4,15 @@ package com.sixdelta;
 public class AlfaStringCalculator
 {
 	int add(String stringToValue){
-		String[] separatedValues = stringToValue.replaceAll("[^\\.0-9]",",").split(",");
+		if(stringToValue.isEmpty()) return 0;
+		String regexValue = "([^\\-.0-9])";
+		String[] separatedValues = stringToValue.replaceAll(regexValue,",").split(",");
 		int aditionResult = 0;
 		for (int i = 0; i<separatedValues.length; i++) {
 			if(separatedValues[i] != "," && !separatedValues[i].equals("")){
 				aditionResult += Integer.parseInt(separatedValues[i]);
 			}
 		}
-		return aditionResult;//Integer.parseInt(n);
+		return aditionResult;
 	}
 }
