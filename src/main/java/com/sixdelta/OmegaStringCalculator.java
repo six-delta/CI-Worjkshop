4fb932a5266e5fdd90eab6f201dce898c0ba71c7
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2017-12-19 11:18:16 -0600

    Pasando la segunda prueba

--- src/main/java/com/sixdelta/OmegaStringCalculator.java
+++ src/main/java/com/sixdelta/OmegaStringCalculator.java
@@ -1,7 +1,12 @@
 package com.sixdelta;
 
-class OmegaStringCalculator{
-  int add(String  x){
-    return 1;
+public class OmegaStringCalculator{
+  public int add(String  numbers){
+  	int i=0;
+  	int sum = 0;
+  	String[] partiendo = numbers.split(",");  
+  	for(i = 0; i < partiendo.length; i++)
+    sum += Integer.parseInt(partiendo[i]);
+	return sum;
   }
 }
