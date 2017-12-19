a3462229f47b63b3e711ef903fd2150e3355dc71
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2017-12-19 11:41:25 -0600

    Pasando test de salto del linea OMEGA

--- src/main/java/com/sixdelta/OmegaStringCalculator.java
+++ src/main/java/com/sixdelta/OmegaStringCalculator.java
@@ -4,7 +4,7 @@ public class OmegaStringCalculator{
   public int add(String  numbers){
   	int i=0;
   	int sum = 0;
-  	String[] partiendo = numbers.split(",");  
+  	String[] partiendo = numbers.split(",|\\n");  
   	for(i = 0; i < partiendo.length; i++)
     sum += Integer.parseInt(partiendo[i]);
 	return sum;
