4a90d8d4dca850b366dfc2989861b6cb19dbf530
Alberto Hernandez <alberto.jhs.33@gmail.com>
2017-12-19 12:18:38 -0600

    Archivos epsilon buenos

--- src/main/java/com/sixdelta/EpsilonStringCalculator.java
+++ src/main/java/com/sixdelta/EpsilonStringCalculator.java
@@ -7,9 +7,6 @@ public class EpsilonStringCalculator {
 
         if(s.isEmpty()) return 0;
 
-<<<<<<< HEAD
-    	return  Stream.of(s.split(",|\\n|//|;"))
-=======
         String delim = ",|\\n";
 
         if(s.startsWith("//")) {
@@ -18,7 +15,6 @@ public class EpsilonStringCalculator {
         }
 
     	return  Stream.of(s.split(delim))
->>>>>>> 0b9075fc969a2d879e45ebb5bf68aeb1e73d649d
                       .mapToInt(Integer::parseInt)
                       .sum();
     }
