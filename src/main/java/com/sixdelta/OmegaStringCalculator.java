75f0566dac667fba53dc5f6817922160ebf400bd
LuisSas <luis@makingdevs.com>
2017-12-19 15:25:32 -0600

    Refactor a codigo de Omega stringcalculator

--- src/main/java/com/sixdelta/OmegaStringCalculator.java
+++ src/main/java/com/sixdelta/OmegaStringCalculator.java
@@ -2,21 +2,40 @@ package com.sixdelta;
 
 public class OmegaStringCalculator{
   public int add(String  numbers){
-  	int i=0;
-  	int sum = 0;
-    if (numbers.startsWith("//")){
-      	String separador = numbers.substring(2,numbers.indexOf("\n"));
-        numbers = numbers.substring(numbers.indexOf("\n")+1);
-      	String[] partiendo = numbers.split(separador);
-        for(i = 0; i < partiendo.length; i++){
-          sum += Integer.parseInt(partiendo[i]);
-        }
+    String separador = ",|\\n";
+    if (theStringStartWithOptionalDelimiter(numbers)){
+      	separador = getTheNewSeparator(numbers);
+        numbers = getNewString(numbers);
+        return getSumOfMyString(separador, numbers);
     }
-    else{
-      String[] partiendo = numbers.split(",|\\n");
-      for(i = 0; i < partiendo.length; i++)
+    else
+      return getSumOfMyString(separador, numbers);
+  
+
+}
+  private int getSumOfMyString(String delimitador, String number){
+    String[] partiendo = number.split(delimitador);
+    int sum = 0;
+    return theSumOfMyString(partiendo, sum);
+  }
+
+  private boolean theStringStartWithOptionalDelimiter(String numbers){
+      return numbers.startsWith("//");
+  }
+
+  private String getTheNewSeparator(String numbers){
+    return numbers.substring(2,numbers.indexOf("\n"));
+  }
+
+  private String getNewString(String numbers){
+    return numbers.substring(numbers.indexOf("\n")+1);
+  }
+
+  private int theSumOfMyString(String[] partiendo, int sum){
+    for(int i = 0; i < partiendo.length; i++){
       sum += Integer.parseInt(partiendo[i]);
     }
-	return sum;
+    return sum;
   }
+  
 }
