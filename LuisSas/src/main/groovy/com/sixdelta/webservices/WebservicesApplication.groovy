20ed96e59b2fa119fa50c3c54350330d0ab96560
LuisSas <luis@makingdevs.com>
2017-12-19 16:10:34 -0600

    Agregando proyecto de webservice

---
+++ LuisSas/src/main/groovy/com/sixdelta/webservices/WebservicesApplication.groovy
@@ -0,0 +1,12 @@
+package com.sixdelta.webservices
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class WebservicesApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run WebservicesApplication, args
+	}
+}
