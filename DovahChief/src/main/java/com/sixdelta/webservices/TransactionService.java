fba3eb36dd738cce1cac5838f2058235d65c9308
DovahChief <pepelimonta@gmail.com>
2017-12-21 11:50:11 -0600

    Ejemplo servicio

--- DovahChief/src/main/java/com/sixdelta/webservices/TransactionService.java
+++ DovahChief/src/main/java/com/sixdelta/webservices/TransactionService.java
@@ -1,5 +1,18 @@
 package com.sixdelta.webservices;
 
-public class TransactionService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.sixdelta.webservices.repository.PersonRepository;
+
+@Service
+public class TransactionService {
+	
+	@Autowired
+	private PersonRepository personRepository;
+	
+	public void executeTransaction(){
+		personRepository.findOne(1l);
+		//personRepository.save(new Person());
+	}
 }
