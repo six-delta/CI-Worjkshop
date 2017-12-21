a80ac147c7c4dbb88c0d730d297a2ae2023437b4
Alberto Hernandez <alberto.jhs.33@gmail.com>
2017-12-21 09:37:00 -0600

    Agregando Person y PersonRepository

---
+++ albertojhs/src/main/java/com/sixdelta/webservices/model/PersonRepository.java
@@ -0,0 +1,16 @@
+package com.sixdelta.webservices.model;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.sixdelta.webservices.model.Person;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends CrudRepository<Person, Long> {
+
+	List<Person> findByName(@Param("name") String name);
+
+}
