1c3f8731b963579a987405f5f78d213e198bcc19
neodevelop <juan@makingdevs.com>
2018-02-05 18:23:20 -0600

    Moving to CrudRepository

--- neodevelop/src/main/java/com/sixdelta/webservices/model/PersonRepository.java
+++ neodevelop/src/main/java/com/sixdelta/webservices/model/PersonRepository.java
@@ -2,12 +2,12 @@ package com.sixdelta.webservices.model;
 
 import java.util.List;
 
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "people", path = "people")
-public interface PersonRepository extends Repository<Person, Long> {
+@RepositoryRestResource(collectionResourceRel = "people", path = "people", exported = true)
+public interface PersonRepository extends CrudRepository<Person, Long> {
 
 	List<Person> findByName(@Param("name") String name);
 	
